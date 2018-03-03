ff7008c4917e7226cf8f2a31f583bd5a17d57760
sebaural <uralsebastian@gmail.com>
2018-03-03 14:32:08 -0500

    trying to change logo size - not working

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <div className="app_logo">
+            <img src={logo} className="logo" alt="logo" />
+          </div>
+          <h2>Welcome to Burger</h2>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
