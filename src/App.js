1d75d6600ede5fdf49f6d312a0061a3c0611de12
ural <sevomax@gmail.com>
2018-03-04 14:19:07 -0500

    added Burger.js

--- src/App.js
+++ src/App.js
@@ -2,19 +2,12 @@ import React, { Component } from 'react';
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <Layout>
-        <div className="App-header">
-          <div className="logo_holder">
-            <img src={logo} className="App-logo" alt="logo"/>
-          </div>
-          <h2>Build React Burger</h2>
-        </div>
         <BurgerBuilder/>
       </Layout>
     );
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
+import logo from '../../logo.svg';
 
-import layoutClasses from './Layout.css';
+import './Layout.css';
 
 const layout = ( props ) => (
 
   <Aux>
     <div>TOOLBAR, SIDE_DRAWER, BACKDROP</div>
-
-    <main className={layoutClasses}>
+    <div className="App-header">
+      <div className="logo_holder">
+        <img src={logo} className="App-logo" alt="logo"/>
+      </div>
+      <h2>Build React Burger</h2>
+    </div>
+    <main>
       {props.children}
     </main>
   </Aux>
