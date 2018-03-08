9143a614a2676106eef5affe134b9d7cc13d0b18
sebaural <uralsebastian@gmail.com>
2018-03-07 19:04:57 -0500

    added order state to Order Now button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -23,7 +23,25 @@ class BurgerBuilder extends React.Component {
       condiments: 0,
       meat: 0
     },
-    totalPrice: 4
+    totalPrice: 4,
+    purchasable: false
+  };
+
+  updatePurchaseState = (ingredients) => {
+/*
+    const ingredients = {
+      ...this.state.ingredients
+    };
+*/
+
+    const sum = Object.keys(ingredients)
+      .map(igKey => {
+        return ingredients[igKey];
+      })
+      .reduce((accum, el) => {
+        return accum + el;
+      }, 0);
+      this.setState({purchasable: sum > 0});
   };
 
   addIngredientHandler = (type) => {
@@ -40,6 +58,7 @@ class BurgerBuilder extends React.Component {
     const newPrice = oldPrice + priceAddition;
 
     this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+    this.updatePurchaseState(updatedIngredients);
   };
 
   removeIngredientHandler = (type) => {
@@ -60,6 +79,7 @@ class BurgerBuilder extends React.Component {
     const newPrice = oldPrice - priceDeduction;
 
     this.setState({totalPrice: newPrice, ingredients: updatedIngredients});
+    this.updatePurchaseState(updatedIngredients);
   };
 
   render() {
@@ -81,6 +101,7 @@ class BurgerBuilder extends React.Component {
           ingredientRemoved={this.removeIngredientHandler}
           disablediabo={disabledInfo}
           price={this.state.totalPrice}
+          purchasable={this.state.purchasable}
         />
         </div>
       </Aux>
