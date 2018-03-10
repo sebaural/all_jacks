162ea7c42088081afa636ccec35923c9f014d666
sebaural <uralsebastian@gmail.com>
2018-03-09 20:14:49 -0500

    added navigation and toolbar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,19 +1,24 @@
 import React from 'react';
 import Aux from '../../hoc/Aux';
-import logo from '../../logo.svg';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import './Layout.css';
 
-import layoutClasses from './Layout.css';
+/*import logo from '../../logo.svg';*/
+
 
 const layout = ( props ) => (
 
   <Aux>
-    <div>TOOLBAR, SIDE_DRAWER, BACKDROP</div>
-    <div className={layoutClasses.App_header}>
-      <div className={layoutClasses.logo_holder}>
-        <img src={logo} className={layoutClasses.App_logo} alt="logo"/>
-      </div>
-      <h2>Build React Burger</h2>
+{/*
+      <div className={layoutClasses.App_header}>
+        <div className={layoutClasses.logo_holder}>
+          <img src={logo} className={layoutClasses.App_logo} alt="logo"/>
+        </div>
+        <h2>Build React Burger</h2>
     </div>
+*/}
+
+    <Toolbar/>
     <main>
       {props.children}
     </main>
---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import classes from './Toolbar.css';
+import logo from '../../../logo.svg';
+
+const Toolbar = ( props ) => {
+  return (
+    <header className={classes.Toolbar}>
+
+      <div className={classes.App_header}>
+        <div className={classes.logo_holder}>
+          <img src={logo} className={classes.App_logo} alt="logo"/>
+        </div>
+        <h2>Build React Burger</h2>
+      </div>
+      
+      <nav>
+        nav ... nav ... nav
+      </nav>      
+      
+    </header>
+  );
+};
+
+export default Toolbar;
