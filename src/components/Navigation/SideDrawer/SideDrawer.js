d988d2dabeb0b02dfaffb67ebad66bdc17d70092
sebaural <uralsebastian@gmail.com>
2018-03-10 20:27:06 -0500

    added SideDrawer js

---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './SideDrawer.css';
+
+const SideDrawer = ( props ) => {
+  return (
+        <div>
+          SideDrawer
+        </div>
+  );
+};
+
+export default SideDrawer;
